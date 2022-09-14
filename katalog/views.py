b3a561922cf7b5ce8741655d23117aac6d0a4fff
dina.dwi11 <dina.dwi11@gmail.com>
2022-09-14 17:46:00 +0700

    done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    database_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': database_katalog,
+        'Nama': 'Dina Dwi Kinanty 🌸',
+        'NPM': '2106751650'
+    }
+    return render(request, "katalog.html", context)
